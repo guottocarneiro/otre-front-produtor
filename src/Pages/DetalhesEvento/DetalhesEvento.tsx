4380dec696ca2832a29075f1e69af096b1cafa2a
Renan <renancossenzo@hotmail.com>
2022-04-27 12:54:11 -0300

    Criando layout da página de detalhes do evento

--- src/Pages/DetalhesEvento/DetalhesEvento.tsx
+++ src/Pages/DetalhesEvento/DetalhesEvento.tsx
@@ -1,9 +1,48 @@
+import './DetalhesEvento.css';
+
 const DetalhesEvento = () => {
-    
+
     return (
         <div className="detalhes-evento">
-            <h1 className="display-3">Nome do evento</h1>
-            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec felis laoreet, euismod velit at, maximus odio. Maecenas sit amet lorem lacinia, suscipit dolor non, porttitor metus. Vestibulum nec finibus orci. Curabitur condimentum volutpat ante. Vestibulum iaculis augue in diam consequat, nec aliquet arcu feugiat.</p>
+            <div>
+                <h1 className="display-3">Nome do evento</h1>
+                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec felis laoreet, euismod velit at, maximus odio.</p>
+            </div>
+
+            <div className="detalhes-evento-informacoes">
+                <p><i className="bi bi-geo-alt"></i> Mineirão - Avenida Teste</p>
+                <p><i className="bi bi-calendar"></i> 22/09/2022</p>
+            </div>
+
+            <div className="detalhes-evento-artistas detalhes-evento-container">
+                <h5>Artistas</h5>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">Duda Beat</li>
+                            <li className="list-group-item">BaianaSystem</li>
+                            <li className="list-group-item">Silva</li>
+                            <li className="list-group-item">Iza</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="detalhes-evento-ingressos detalhes-evento-container">
+                <h5>Ingressos</h5>
+
+                <div className="row">
+                    <div className="col-md-6">
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item">A list item</li>
+                            <li className="list-group-item">A list item</li>
+                            <li className="list-group-item">A list item</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
