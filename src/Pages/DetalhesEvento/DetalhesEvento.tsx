757808023625f7fc4cb257d0596972718fe73042
Renan <renancossenzo@hotmail.com>
2022-04-27 13:24:12 -0300

    Preenchendo detalhes do evento

--- src/Pages/DetalhesEvento/DetalhesEvento.tsx
+++ src/Pages/DetalhesEvento/DetalhesEvento.tsx
@@ -1,17 +1,71 @@
+import { useEffect, useState } from 'react';
+import Evento from '../../interfaces/evento.interface';
+import obterDescricaoEndereco from '../../utils/descricao-endereco.utils';
 import './DetalhesEvento.css';
 
 const DetalhesEvento = () => {
 
+    const estadoInicial: Evento = {
+        artistas: [],
+        data: '',
+        descricao: '',
+        endereco: {
+            bairro: '',
+            cidade: '',
+            complemento: undefined,
+            logradouro: '',
+            nome: '',
+            numero: '',
+            uf: ''
+        },
+        ingressos: [],
+        nome: ''
+    }
+    const [evento, setEvento] = useState<Evento>(estadoInicial);
+
+    useEffect(() => {
+        const novoEvento: Evento = {
+            artistas: [
+                { nome: 'Duda Beat' },
+                { nome: 'BaianaSystem' },
+                { nome: 'Djonga' },
+                { nome: 'Iza' },
+                { nome: 'Silva' },
+                { nome: 'Glória Groove' }
+            ],
+            data: '03/09/2022',
+            descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec felis laoreet, euismod velit at, maximus odio.',
+            endereco: {
+                bairro: 'Pampulha',
+                cidade: 'Belo Horizonte',
+                complemento: undefined,
+                logradouro: 'Avenida Presidente Carlos Luz',
+                nome: 'Mineirão',
+                numero: '1921',
+                uf: 'MG'
+            },
+            ingressos: [
+                { quantidade: 4000, tipo: 'Inteira', valor: 200 },
+                { quantidade: 1000, tipo: 'Meia', valor: 100 },
+                { quantidade: 1000, tipo: 'Meia solidária', valor: 200 },
+                { quantidade: 500, tipo: 'Camarote', valor: 350 }
+            ],
+            nome: 'Evento do Renan'
+        };
+
+        setEvento(novoEvento);
+    }, [])
+
     return (
         <div className="detalhes-evento">
             <div>
-                <h1 className="display-3">Nome do evento</h1>
-                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nec felis laoreet, euismod velit at, maximus odio.</p>
+                <h1 className="display-3">{evento.nome}</h1>
+                <p className="lead">{evento.descricao}</p>
             </div>
 
             <div className="detalhes-evento-informacoes">
-                <p><i className="bi bi-geo-alt"></i> Mineirão - Avenida Teste</p>
-                <p><i className="bi bi-calendar"></i> 22/09/2022</p>
+                <p><i className="bi bi-geo-alt"></i> { obterDescricaoEndereco(evento.endereco) }</p>
+                <p><i className="bi bi-calendar"></i> {evento.data}</p>
             </div>
 
             <div className="detalhes-evento-artistas detalhes-evento-container">
@@ -20,10 +74,13 @@ const DetalhesEvento = () => {
                 <div className="row">
                     <div className="col-md-6">
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Duda Beat</li>
-                            <li className="list-group-item">BaianaSystem</li>
-                            <li className="list-group-item">Silva</li>
-                            <li className="list-group-item">Iza</li>
+                            {
+                                evento.artistas.map((art, i) => {
+                                    return (
+                                        <li key={i} className="list-group-item">{art.nome}</li>
+                                    );
+                                })
+                            }
                         </ul>
                     </div>
                 </div>
@@ -35,10 +92,18 @@ const DetalhesEvento = () => {
 
                 <div className="row">
                     <div className="col-md-6">
-                        <ul className="list-group list-group-flush">
-                            <li className="list-group-item">A list item</li>
-                            <li className="list-group-item">A list item</li>
-                            <li className="list-group-item">A list item</li>
+                        <ul className="list-group">
+                            {
+                                evento.ingressos.map((ing, i) => {
+                                    return (
+                                        <li key={i} className="list-group-item">
+                                            <strong>{ing.tipo}</strong><br />
+                                            {ing.valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}<br />
+                                            Quantidade: {ing.quantidade.toLocaleString('pt-br')}
+                                        </li>
+                                    );
+                                })
+                            }
                         </ul>
                     </div>
                 </div>
